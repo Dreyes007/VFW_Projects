95bbf9739b954b8f574ea0298b55b376ab8b76c9
Daniel Reyes <Dreyes007@fullsail.edu>
2012-10-30 23:03:56 -0400

    Added Choose a menu function to Main.js file

--- Main.js
+++ Main.js
@@ -11,22 +11,38 @@ window.addEventListener("DOMContentLoaded", function(){
 		var theElement = document.getElementById(x);
 		return theElement;		
 	}
+		//Select field element populated with options
+	function chooseMenu(){
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $('select'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "menus");
+		for(var i=0, j=menuGroups.length; i<j; i++){
+			var makeOption = document.createElement('option');
+			var optText = menuGroups[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);		
+	}
 	
 		
 	//Variable default
 	var menuGroups = ["--Choose A Menu--", "Appetizer", "Entree", "Dessert"];
-	
+	chooseMenu();
 	
 	
 	
 	//Set link & Submit Click Events
+/*
 	var displayLink = $('displayLink');
 	displayLink.addEventListener("click", getData);
 	var clearLink = $('clear');
 	clearLink.addEventListener("click", clearLocal);
 	var save = $('submit');
 	save.addEventListener("click", storeData);
-
+*/
 
 
 

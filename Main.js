bb0d72604541f03bc7d9bb0820b64c782cfc6622
Daniel Reyes <Dreyes007@fullsail.edu>
2012-10-31 00:43:31 -0400

    Added part of the store data function and made a few changes to the addItem.html file.

--- Main.js
+++ Main.js
@@ -27,7 +27,26 @@ window.addEventListener("DOMContentLoaded", function(){
 		selectLi.appendChild(makeSelect);		
 	}
 	
-		
+	function storeData(key){		
+		var id 				= Math.floor(Math.random()*100000001);
+		//Gather up all our form field values and store in an object.
+		//Object properties contain array with the form label and input value.
+		var item			= {};
+			item.fname		= ["First Name:", $('fname').value];
+			item.lname		= ["Last Name:", $('lname').value];	
+			item.email		= ["Email:", $('email').value];
+			item.phone		= ["Phone Number:", $('phone').value];
+			item.address	= ["Address:", $('address').value];
+			item.order		= ["Order:", orderValue];
+			item.payment	= ["Payment", paymentValue];
+			item.select 	= ["Menus:", $('select').value];
+			item.amount 	= ["Order Amount:", $('amount').value];
+			item.date		= ["Delivery Date:", $('date').value];
+			item.comments	= ["Additional Instructions:", $('comments').value];
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Order Saved!");
+	}
+			
 	//Variable default
 	var menuGroups = ["--Choose A Menu--", "Appetizer", "Entree", "Dessert"];
 	chooseMenu();
@@ -39,10 +58,10 @@ window.addEventListener("DOMContentLoaded", function(){
 	var displayLink = $('displayLink');
 	displayLink.addEventListener("click", getData);
 	var clearLink = $('clear');
-	clearLink.addEventListener("click", clearLocal);
+	clearLink.addEventListener("click", clearLocal);*/
 	var save = $('submit');
 	save.addEventListener("click", storeData);
-*/
+
 
 
 
